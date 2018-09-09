1465f27d5a40efced3f7b941b883f6163840de3a
Dan Amador <danamadorpe@gmail.com>
2018-09-09 00:24:12 -0500

    added gif

--- src/Views/Main.java
+++ src/Views/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("mainScene.fxml"));
         primaryStage.setTitle("FotoChopz");
-        primaryStage.setScene(new Scene(root, 1000, 900));
+        primaryStage.setScene(new Scene(root, 900, 300));
         primaryStage.show();
     }
 
